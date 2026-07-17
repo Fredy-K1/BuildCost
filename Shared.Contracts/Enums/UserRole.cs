ea4785ff9df7bd6d69006d359c9ad1cbc3b5b7ed
Marco Antonio Reyes Perez <marco.reyes@itsvc.edu.mx>
2026-07-17 01:56:13 -0600

    CustomerService

---
+++ Shared.Contracts/Enums/UserRole.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Shared.Contracts.Enums;
+public enum UserRole
+{
+    Admin = 1,
+    Cliente = 2,
+    Contratista = 3
+}
+
---
+++ Shared.Contracts/Services/JwtService.cs
@@ -0,0 +1,31 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Shared.Contracts.Entities;
+
+namespace Shared.Contracts.Services;
+
+public static class JwtService
+{
+    public static string GenerateToken(User user, string key)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role.ToString())
+            }),
+            Expires = DateTime.UtcNow.AddHours(2),
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
