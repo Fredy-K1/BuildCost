2120721a37266883dd8a71a5ce8147261a54b565
Marco Antonio Reyes Perez <marco.reyes@itsvc.edu.mx>
2026-07-17 01:49:10 -0600

    Backend inicial CustomerService

--- Shared.Contracts/DTOs/Auth/RecoverRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace Shared.Contracts.DTOs.Auth;
-
-public class RecoverRequest
-{
-    public string Email { get; set; } = string.Empty;
-    public string NewPassword { get; set; } = string.Empty;
-}
--- Shared.Contracts/DTOs/Auth/UpdateProfileRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared.Contracts.DTOs.Auth;
-
-public class UpdateProfileRequest
-{
-    public string Name { get; set; } = string.Empty;
-    public string Apaterno { get; set; } = string.Empty;
-    public string Amaterno { get; set; } = string.Empty;
-    public string Telefono { get; set; } = string.Empty;
-}
