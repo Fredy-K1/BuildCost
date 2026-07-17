83205e2cc7eea31e531aaf19071392076b49fd3d
Marco Antonio Reyes Perez <marco.reyes@itsvc.edu.mx>
2026-07-17 01:33:50 -0600

    Agrego backend CustomerService

---
+++ Shared.Contracts/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Contracts.Entities;
+
+namespace Shared.Contracts.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<User> Users => Set<User>();
+}
